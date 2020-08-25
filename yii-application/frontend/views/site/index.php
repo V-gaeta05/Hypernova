61d719fcbfa9edfa65bc5891de0587485e61da85
Daniel <dioclo90@gmail.com>
2020-08-25 15:23:59 +0200

    p

--- yii-application/frontend/views/site/index.php
+++ yii-application/frontend/views/site/index.php
@@ -54,5 +54,33 @@ if(Yii::$app->user->isGuest){
     </div>
 </div>
 <?php }else{ ?>
-
+    <div id="paypal-button-container"></div>
 <?php } ?>
+<script
+    src="https://www.paypal.com/sdk/js?client-id=AW2t8HdbPQ17rEBOimuBMQKKIei1xXIiR3cSEBDybdy0gdMksYx40KMtM1RO6WytiH8yYnhAxZRHlTb4"> // Required. Replace SB_CLIENT_ID with your sandbox client ID.
+    </script>
+<script>
+  paypal.Buttons({
+    createOrder: function(data, actions) {
+      // This function sets up the details of the transaction, including the amount and line item details.
+      return actions.order.create({
+        purchase_units: [{
+          amount: {
+            value: '<?php echo Yii::$app->user->getId();?>',
+            currency: 'EUR'
+          },
+          description: 'Nome descrizione'
+        }]
+      });
+    },
+    onApprove: function(data, actions) {
+      // This function captures the funds from the transaction.
+      return actions.order.capture().then(function(details) {
+        // This function shows a transaction success message to your buyer.
+
+        window.location = "index.php";
+      });
+    }
+  }).render('#paypal-button-container');
+  //This function displays Smart Payment Buttons on your web page.
+</script>
